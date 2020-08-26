f90d7762e7f10352ece31fc7ef3e50023c58cae1
Steven <venturizhou@gmail.com>
2020-08-26 08:40:45 -0400

    update

---
+++ wcat.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char * argv[]) {
+    cout << argv[1] << endl;
+    // FILE *temp; 
+    // char line[200];
+    // temp = fopen(argv[0],"r");
+    // if(temp==NULL){
+    //     cout << "Cannot open file" << endl;
+    //     exit(1);
+    // }
+    // cout << "Reading File" << endl;
+    // while(fgets (line,200,temp)!=NULL)
+    // cout << line << endl;
+    // cout << "Closing File" << endl;
+    // fclose(temp);
+
+
+  return 0;   
+}
